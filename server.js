2506b25cd880c529922090d0690e9d5ad8dd2660
Dine-Dhamo <dinesh.dhamotharann@gmail.com>
2026-01-29 18:26:26 +0530

    fixed cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 import { healthz } from './src/routes/healthz.js';
 import { createPaste } from './src/routes/createPaste.js';
@@ -7,8 +8,12 @@ import { viewPaste } from './src/routes/viewPaste.js';
 import { url } from './src/utils/constants.js';
 
 const app = express();
+app.use(cors({
+  origin:'*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'x-test-now-ms'],
+}));
 app.use(express.json());
-
 app.get(url.HEALTHZ, healthz);
 app.post(url.PASTES, createPaste);
 app.get(url.FETCH_PASTES, fetchPaste);
