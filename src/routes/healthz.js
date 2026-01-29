a93a27bd9e08544dacefcbb68cfb1f354e098a81
Dine-Dhamo <dinesh.dhamotharann@gmail.com>
2026-01-29 12:44:10 +0530

    modification done

--- src/routes/healthz.js
+++ src/routes/healthz.js
@@ -1,12 +1,18 @@
-import { prisma } from '../lib/prisma.js';
+
+import { getPrisma } from '../lib/prisma.js';
 
 export async function healthz(req, res) {
-  console.log(process.env.DATABASE_URL)
+  console.log('DATABASE_URL:', process.env.DATABASE_URL);
+
   try {
+    const prisma = getPrisma();
     await prisma.$queryRaw`SELECT 1`;
     res.status(200).json({ ok: true });
   } catch (err) {
     console.error('Healthz DB error:', err);
-    res.status(500).json({ ok: false });
+    res.status(500).json({
+      ok: false,
+    });
   }
 }
+
